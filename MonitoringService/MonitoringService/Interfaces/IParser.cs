0268eb72dc681ae543383128be5653e9684ccf0b
garamreka <garam.reka@gmail.com>
2019-01-11 21:44:10 +0100

    feat(MonitoringService): extend interfaces

---
+++ MonitoringService/MonitoringService/Interfaces/IParser.cs
@@ -0,0 +1,22 @@
+﻿using MonitoringService.Models;
+
+namespace MonitoringService.Interfaces
+{
+    /// <summary>
+    /// Implementation of IPareser interface
+    /// </summary>
+    public interface IParser
+    {
+        /// <summary>
+        /// Reads all line of a text file
+        /// </summary>
+        /// <returns>With the lines of the file</returns>
+        string[] ReadFile();
+
+        /// <summary>
+        /// Parses the file content to ServiceParameter
+        /// </summary>
+        /// <returns>With the ServiceParameter</returns>
+        ServiceParameter ParseToParameter();
+    }
+}
--- MonitoringService/MonitoringService/Interfaces/IRepository.cs
+++ MonitoringService/MonitoringService/Interfaces/IRepository.cs
@@ -1,11 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace MonitoringService.Interfaces
 {
-    interface IRepository
+    /// <summary>
+    /// Implementation of IRepository interface
+    /// </summary>
+    public interface IRepository<T>
     {
+        /// <summary>
+        /// Gets all items
+        /// </summary>
+        /// <returns>With the Items</returns>
+        IEnumerable<T> GetAllItem();
+
+        /// <summary>
+        /// Gets the item based on id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>With the Item</returns>
+        T GetItemById(int id);
     }
 }
