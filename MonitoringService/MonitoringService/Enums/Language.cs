3ccfeb18f041df24cf2a2dd2bc9ff71f6302a0b6
garamreka <garam.reka@gmail.com>
2019-01-12 22:54:13 +0100

    feat(MonitoringService.Enums): add enum for valid languages

---
+++ MonitoringService/MonitoringService/Enums/Language.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonitoringService.Enums
+{
+    /// <summary>
+    /// Implementation of Language enum
+    /// </summary>
+    public enum Language
+    {
+        /// <summary>
+        /// Estonian
+        /// </summary>
+        Estonian,
+
+        /// <summary>
+        /// English
+        /// </summary>
+        English,
+
+        /// <summary>
+        /// Undefined
+        /// </summary>
+        Undefined
+    }
+}
