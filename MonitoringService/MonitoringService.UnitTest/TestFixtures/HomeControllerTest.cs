b157dad46d3944c883628971a7947753af89f5ab
garamreka <garam.reka@gmail.com>
2019-01-13 20:06:40 +0100

    feat(MonitoringService.UnitTest): implement HomeControllerTest class

--- MonitoringService/MonitoringService.UnitTest/TestFixtures/HomeControllerTest.cs
+++ MonitoringService/MonitoringService.UnitTest/TestFixtures/HomeControllerTest.cs
@@ -1,10 +1,56 @@
-﻿using System;
+﻿using MonitoringService.Controllers;
+using MonitoringService.Interfaces;
+using MonitoringService.Models;
+using Moq;
+using NUnit.Framework;
 using System.Collections.Generic;
-using System.Text;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MonitoringService.UnitTest.TestFixtures
 {
-    class HomeControllerTest
+    /// <summary>
+    /// Test class for HomeController
+    /// </summary>
+    [TestFixture]
+    public class HomeControllerTest : ServiceTestBase
     {
+        #region Fields
+
+        private readonly Mock<IRepository<Service>> _mockServiceRepository;
+        private readonly HomeController _homeController;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public HomeControllerTest()
+        {
+            _mockServiceRepository = new Mock<IRepository<Service>>();
+            _homeController = new HomeController(_mockServiceRepository.Object);
+        }
+
+        #endregion
+
+        #region Tests
+
+        /// <summary>
+        /// Tests the Index action
+        /// </summary>
+        [Test]
+        public void Index_Returns_ServiceJson() //todo
+        {
+            _mockServiceRepository
+                .Setup(repo => repo.GetItemById(It.IsAny<int>()))
+                .Returns(ActiveService);
+
+            var result = (JsonResult)_homeController.Index();
+
+            Assert.AreEqual(ActiveService, (Service)result.Value);
+        }
+
+        #endregion
     }
 }
